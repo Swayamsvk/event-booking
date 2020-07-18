d9083a085142bf974dcde627b3fb3896ea037dfd
Swayam Samyak Debasis <you@example.com>
2020-07-18 21:36:32 +0530

    added authentication

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import React from 'react';
-import {BrowserRouter as Router,Route} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import Navbar from './components/Navbar/navbar.component';
-import CreateEvent from './components/create-event.component';
-import EditEvent from './components/edit-event.component';
-import EventList from './components/Events-List/event-list.component';
-// import Register from './components/register.component';
-// import Login from './components/login.component';
+import "./App.css";
+import Navbar from "./components/Navbar/navbar.component";
+import CreateEvent from "./components/create-event.component";
+import EditEvent from "./components/edit-event.component";
+import EventList from "./components/Events-List/event-list.component";
+import Register from "./components/register.component";
+import Login from "./components/login.component";
+import Alerts from "./components/layout/Alerts";
+import setAuthToken from "./utils/setAuthToken";
+import PrivateRoute from "./components/routing/PrivateRoute";
+import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 
-
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 function App() {
   return (
-  <Router>
-    <div className="container">
-      <Navbar />
-        <br/>
-        <Route path="/" exact component={EventList}/>
-        <Route path="/edit/:id" component={EditEvent}/>
-        <Route path="/create" component={CreateEvent}/>
-        {/* <Route path="/register" component={Register}/> */}
-        {/* <Route path="/login" component={Login}/> */}
-    </div>
-  </Router>  
-    
+    <AuthState>
+      <AlertState>
+        <Router>
+          <div className="container">
+            <Navbar />
+            <br />
+            <PrivateRoute exact path="/" exact component={EventList} />
+            <Route exact path="/edit/:id" component={EditEvent} />
+            <Route exact path="/create" component={CreateEvent} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </Router>
+      </AlertState>
+    </AuthState>
   );
 }
 
--- src/components/Navbar/navbar.component.js
+++ src/components/Navbar/navbar.component.js
@@ -1,37 +1,117 @@
-import React,{Component} from 'react';
-import {Link} from 'react-router-dom';
-import './navbar.css';
+import React, { Fragment, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import AuthContext from "../../context/auth/authContext";
 
+const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
 
-export default class Navbar extends Component{
-    render(){
-        return(
-            <nav>
-                <div>
-                <Link to="/" className="link_main nav-link" id="main">Events Booking</Link>
-                </div>
-                <div>
-              <ul>
-                <li>
-                    <Link to="/" className="nav-link link">Events</Link>
-                </li>
-                <li>
-                  <Link to="/create" className="nav-link link">Create Event Log</Link>
-                </li>
-                {/* <li className="nav-item">
-                  <Link to="/register" className="nav-link">Register</Link>
-                </li> */}
-                {/* <li className="nav-item">
-                  <Link to="/login" className="nav-link">Login</Link>
-                </li> */}
-                {/* <li className="nav-item">
-                  <Link to="/logout" className="nav-link">Logout</Link>
-                </li> */}
-                
-              </ul>
-            </div>
-          </nav>
-        );
-    }
-}
+  const { isAuthenticated, logout, user } = authContext;
 
+  const onLogout = () => {
+    logout();
+  };
+
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name}</li>
+      <div>
+        <Link to="/" className="link_main nav-link" id="main">
+          Events Booking
+        </Link>
+      </div>
+      <li>
+        <Link to="/" className="nav-link link">
+          Events
+        </Link>
+      </li>
+      <li>
+        <Link to="/create" className="nav-link link">
+          Create Event Log
+        </Link>
+      </li>
+      <li>
+        <a onClick={onLogout} href="#!">
+          <i className="fas fa-sign-out-alt"></i>{" "}
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/register" className="nav-link link">
+          Register
+        </Link>
+      </li>
+      <li>
+        <Link to="/login" className="nav-link link">
+          Login
+        </Link>
+      </li>
+    </Fragment>
+  );
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon} />
+        {title}
+      </h1>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: "Contact Keeper",
+  icon: "fas fa-id-card-alt",
+};
+export default Navbar;
+
+// import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+// import "./navbar.css";
+
+// export default class Navbar extends Component {
+//   render() {
+//     return (
+//       <nav>
+//         <div>
+//           <Link to="/" className="link_main nav-link" id="main">
+//             Events Booking
+//           </Link>
+//         </div>
+//         <div>
+//           <ul>
+//             <li>
+//               <Link to="/" className="nav-link link">
+//                 Events
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="/create" className="nav-link link">
+//                 Create Event Log
+//               </Link>
+//             </li>
+//             {/* <li className="nav-item">
+//                   <Link to="/register" className="nav-link">Register</Link>
+//                 </li> */}
+//             {/* <li className="nav-item">
+//                   <Link to="/login" className="nav-link">Login</Link>
+//                 </li> */}
+//             {/* <li className="nav-item">
+//                   <Link to="/logout" className="nav-link">Logout</Link>
+//                 </li> */}
+//           </ul>
+//         </div>
+//       </nav>
+//     );
+//   }
+// }
